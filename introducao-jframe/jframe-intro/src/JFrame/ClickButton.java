7f909c57016ed28a2081d5971e72ed5f356278b3
psicoder0101 <pedrocsouza97@gmail.com>
2024-07-23 15:47:48 -0300

    initialized project intro to jframe studying

---
+++ introducao-jframe/jframe-intro/src/JFrame/ClickButton.java
@@ -0,0 +1,14 @@
+package JFrame;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class ClickButton extends JButton {
+    public ClickButton (String text){
+        setText(text);
+        setBounds(65, 150, 250, 50);
+        setFont(new Font("Arial", Font.BOLD,20));
+        setForeground(new Color(0));
+        setBackground(new Color(0x006DFF));
+    }
+}

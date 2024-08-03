785efabd20efcac3fb8747be42888b2b91e8f694
Pedro Souza <pedrocsouza97@gmail.com>
2024-08-02 22:03:28 -0300

    Delete introducao-jframe directory

--- introducao-jframe/TelaLoginSwing/src/application/Main.java
+++
@@ -1,9 +0,0 @@
-package application;
-
-import view.loginScreen;
-
-public class Main {
-    public static void main(String[] args) {
-        new loginScreen();
-    }
-}
--- introducao-jframe/jframe-intro/src/Main.java
+++
@@ -1,9 +0,0 @@
-import JFrame.Screen;
-
-public class Main {
-    public static void main(String[] args) {
-
-        new Screen("teste", 400, 300);
-    }
-
-}

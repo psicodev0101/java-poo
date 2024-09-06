618fd701abf05d1128e226d743e6e9fa41fb0bfa
psicodev0101 <pedrocsouza97@gmail.com>
2024-09-05 21:10:27 -0300

    edited CobrancaImpCpfServiceTest class ok

--- quatroPilares/br/com/projetoLeao/tests/services/CobrancaImpCpfServiceTest.java
+++ quatroPilares/br/com/projetoLeao/tests/services/CobrancaImpCpfServiceTest.java
@@ -11,15 +11,16 @@ class CobrancaImpCpfServiceTest {
 
     //contribuinte tipo cpf
     Contribuinte contribTesteA = new ContribuinteCpf("Adailson", 1500D, 250D);
+    Contribuinte contribTesteB = new ContribuinteCpf("Adalberto", 2500D, 500D);
+    Contribuinte contribTesteC = new ContribuinteCpf("Adalvano", 4500D, 340D);
 
-    //contribuinte tipo cnpj
-    Contribuinte contribTesteD = new ContribuinteCnpj("OdontoVida", 52000D, 50);
 
     @Test
     void calcularImpostoTest() {
         //teste de calculo para imposto de contribuinte cpf
         assertEquals(100D, new CobrancaImpCpfService().calcularImposto(contribTesteA));
-        //teste de calculo para imposto de contribuinte cnpj
-        assertEquals(7280D, new CobrancaImpCnpjService().calcularImposto(contribTesteD));
+        assertEquals(375D, new CobrancaImpCpfService().calcularImposto(contribTesteB));
+        assertEquals(955D, new CobrancaImpCpfService().calcularImposto(contribTesteC));
     }
+
 }

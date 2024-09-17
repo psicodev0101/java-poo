a1e90fb275d7106552e851a4dab499b8528ed6b2
psicoder0101 <pedrocsouza97@gmail.com>
2024-09-17 13:27:37 -0300

    initialized Conta class

--- quatroPilares/projetoBanco/src/models/Conta.java
+++ quatroPilares/projetoBanco/src/models/Conta.java
@@ -1,4 +1,29 @@
 package models;
 
-public class Conta {
+public abstract class Conta {
+
+    private final int numeroConta;
+    private final String titularConta;
+    private double saldoConta;
+
+    public Conta (int numeroConta, String titularConta){
+        this.numeroConta = numeroConta;
+        this.titularConta = titularConta;
+    }
+
+    public int getNumeroConta() {
+        return numeroConta;
+    }
+
+    public String getTitularConta() {
+        return titularConta;
+    }
+
+    public double getSaldoConta() {
+        return saldoConta;
+    }
+
+    public abstract void depositar (double valor);
+    public abstract void sacar (double valor);
+
 }
